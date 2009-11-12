d97a318c2f76659b1fc31f0385f79b602ff02555
Ash <ash@ash-laptop.(none)>
2009-11-12 16:18:37 +1100

    removed cart index override as not necessary

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,7 @@ map.resources :orders do |order|
   # not being able to create the payment before sending the request to paypal
   order.resources :paypal_payments, :collection => {:successful => :post}
 end
+
+map.namespace :admin do |admin|
+  admin.resources :orders, :has_many => [:paypal_payments]
+end
