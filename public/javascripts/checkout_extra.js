08f06651b11c1b159d7e47c70751820d5b3b1f51
ashcrick <ash@or2b.com>
2009-11-20 17:50:16 +1100

    put the shipping on a separate line item in PayPal.  Still need to test IPN

--- public/javascripts/checkout_extra.js
+++ public/javascripts/checkout_extra.js
@@ -1,19 +1,32 @@
-$(function() { 
-    $('#continue_billing').click(function() {
-        $('input#hidden_sstate').val($('input#hidden_bstate').val());
-        $("#billing input, #billing select").each(function() {
-            $("#shipping #"+ $(this).attr('id').replace('bill', 'shipment_attributes')).val($(this).val());
-        })
-        submit_shipping();
-    })
+$(function() {
+    var old_continue_section = continue_section;
 
-//    $('#continue_confirmation').click(function() {
-//        $('input#hidden_sstate').val($('input#hidden_bstate').val());
-//        $("#billing input, #billing select").each(function() {
-//            $("#shipping #"+ $(this).attr('id').replace('bill', 'shipment_attributes')).val($(this).val());
-//        })
-//        submit_shipping();
-//        return false;
-//    })
+    continue_section = function(section) {
+        if (section == 'billing') {
+            $('input#hidden_sstate').val($('input#hidden_bstate').val());
+            $("#billing input, #billing select").each(function() {
+                $("#shipping #"+ $(this).attr('id').replace('bill', 'shipment_attributes')).val($(this).val());
+            })
+            var result = old_continue_section(section);
+            submit_shipping();
+            return result;
+        } else
+            return old_continue_section(section);
+    }
 
+    var old_update_confirmation = update_confirmation
+
+    update_confirmation = function(order) {
+        var last_index = $('#last_index').val();
+        $('#country').val($('#checkout_bill_address_attributes_country_id :selected').text());
+        var shipping = "$0.00";
+        for (var key in order.charges) {
+            if (key != "Tax")
+                shipping = order.charges[key];
+        }
+        $('#item_name_' + last_index).val("Delivery");
+        $('#amount_' + last_index).val(shipping);
+        $('#paypal_enter').show();
+        return old_update_confirmation(order);
+    }
 })
